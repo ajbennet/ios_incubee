274d5f0c260a8bab5f2f9ca39fc54e67147e3399
Rithesh Rao <rithesh@incub.ee>
2015-08-26 23:16:33 +0530

    Chatviewcontroller.

---
+++ Incubee/Incubee/Source/OperationManagers/ICMessengerManager.h
@@ -0,0 +1,13 @@
+//
+//  ICMessengerManager.h
+//  Incubee
+//
+//  Created by Rithesh Rao on 26/08/15.
+//  Copyright (c) 2015 Incubee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ICMessengerManager : NSObject
+
+@end
---
+++ Incubee/Incubee/Source/OperationManagers/ICMessengerManager.m
@@ -0,0 +1,13 @@
+//
+//  ICMessengerManager.m
+//  Incubee
+//
+//  Created by Rithesh Rao on 26/08/15.
+//  Copyright (c) 2015 Incubee. All rights reserved.
+//
+
+#import "ICMessengerManager.h"
+
+@implementation ICMessengerManager
+
+@end
