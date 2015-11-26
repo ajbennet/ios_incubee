21fa6f52d3e90de979760649bad9152f9b359935
Rithesh Rao <rithesh@incub.ee>
2015-11-26 20:08:32 +0530

    Server API Version Updated.

--- Incubee/Incubee/Source/Network Module/ICNetworkConstant.h
+++ Incubee/Incubee/Source/Network Module/ICNetworkConstant.h
@@ -16,37 +16,41 @@
 
 #define SERVER_HOST                 @"www.incub.ee"
 
+#define SERVER_API_VERSION @"v1.0"
+
 #elif RELEASE
 
 #define SERVER_REST_PROTOCOL        @"http://"
 
 #define SERVER_HOST                 @"www.incub.ee"
 
+#define SERVER_API_VERSION @"v1.0"
+
 #endif
 
 #pragma mark - REST URL -
 
-#define kGetAllCompanyURL  [NSString stringWithFormat:@"%@%@/rest/all",SERVER_REST_PROTOCOL,SERVER_HOST]
+#define kGetAllCompanyURL  [NSString stringWithFormat:@"%@%@/rest/%@/all",SERVER_REST_PROTOCOL,SERVER_HOST,SERVER_API_VERSION]
 
-#define kGoogleLoginURL  [NSString stringWithFormat:@"%@%@/rest/login",SERVER_REST_PROTOCOL,SERVER_HOST]
+#define kGoogleLoginURL  [NSString stringWithFormat:@"%@%@/rest/%@/login",SERVER_REST_PROTOCOL,SERVER_HOST,SERVER_API_VERSION]
 
-#define kGoogleSignUpURL  [NSString stringWithFormat:@"%@%@/rest/signup",SERVER_REST_PROTOCOL,SERVER_HOST]
+#define kGoogleSignUpURL  [NSString stringWithFormat:@"%@%@/rest/%@/signup",SERVER_REST_PROTOCOL,SERVER_HOST,SERVER_API_VERSION]
 
-#define kLikeIncubeeURL(incubeeId,userId) [NSString stringWithFormat:@"%@%@/rest/like/%@?uid=%@",SERVER_REST_PROTOCOL,SERVER_HOST,incubeeId,userId]
+#define kLikeIncubeeURL(incubeeId,userId) [NSString stringWithFormat:@"%@%@/rest/%@/like/%@?uid=%@",SERVER_REST_PROTOCOL,SERVER_HOST,SERVER_API_VERSION,incubeeId,userId]
 
-#define kAddCustomer(incubeeId,userId) [NSString stringWithFormat:@"%@%@/rest/customer/%@?uid=%@",SERVER_REST_PROTOCOL,SERVER_HOST,incubeeId,userId]
+#define kAddCustomer(incubeeId,userId) [NSString stringWithFormat:@"%@%@/rest/%@/customer/%@?uid=%@",SERVER_REST_PROTOCOL,SERVER_HOST,SERVER_API_VERSION,incubeeId,userId]
 
-#define kAllLikedIncubees(userId) [NSString stringWithFormat:@"%@%@/rest/like?id=%@",SERVER_REST_PROTOCOL,SERVER_HOST,userId]
+#define kAllLikedIncubees(userId) [NSString stringWithFormat:@"%@%@/rest/%@/like?id=%@",SERVER_REST_PROTOCOL,SERVER_HOST,SERVER_API_VERSION,userId]
 
-#define kAllCustomerIncubees(userId) [NSString stringWithFormat:@"%@%@/rest/customer?id=%@",SERVER_REST_PROTOCOL,SERVER_HOST,userId]
+#define kAllCustomerIncubees(userId) [NSString stringWithFormat:@"%@%@/rest/%@/customer?id=%@",SERVER_REST_PROTOCOL,SERVER_HOST,SERVER_API_VERSION,userId]
 
-#define kCustomerDetails(customerId) [NSString stringWithFormat:@"%@%@/rest/v1.0/customer/details?id=%@",SERVER_REST_PROTOCOL,SERVER_HOST,customerId]
+#define kCustomerDetails(customerId) [NSString stringWithFormat:@"%@%@/rest/%@/customer/details?id=%@",SERVER_REST_PROTOCOL,SERVER_HOST,SERVER_API_VERSION,customerId]
 
-#define kGetAllChatMsg(userId) [NSString stringWithFormat:@"%@%@/rest/msg/all?eid=%@",SERVER_REST_PROTOCOL,SERVER_HOST,userId]
+#define kGetAllChatMsg(userId) [NSString stringWithFormat:@"%@%@/rest/%@/msg/all?eid=%@",SERVER_REST_PROTOCOL,SERVER_HOST,SERVER_API_VERSION,userId]
 
-#define kGetAllFounderChatMsg(userId) [NSString stringWithFormat:@"%@%@/rest/msg/all?eid=%@",SERVER_REST_PROTOCOL,SERVER_HOST,userId]
+#define kGetAllFounderChatMsg(userId) [NSString stringWithFormat:@"%@%@/rest/%@/msg/all?eid=%@",SERVER_REST_PROTOCOL,SERVER_HOST,SERVER_API_VERSION,userId]
 
-#define ksendChatMsg(userId) [NSString stringWithFormat:@"%@%@/rest/msg?eid=%@",SERVER_REST_PROTOCOL,SERVER_HOST,userId]
+#define ksendChatMsg(userId) [NSString stringWithFormat:@"%@%@/rest/%@/msg?eid=%@",SERVER_REST_PROTOCOL,SERVER_HOST,SERVER_API_VERSION,userId]
 
 #pragma mark - Request Method -
 
